867bc58646ccfd2f3c99595b6a2f0b0ff9921a1c
Monica <mcarvajalr@icloud.com>
2025-06-12 15:36:34 -0400

    Initialize saludoapp project with Maven configuration, main application, and unit tests

---
+++ src/test/java/com/equipo/saludo/AppTest.java
@@ -0,0 +1,41 @@
+package com.equipo.saludo;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase {
+
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp() {
+        assertTrue(true);
+    }
+
+    /**
+     * Custom test for App.saludar
+     */
+    public void testSaludo() {
+        assertEquals("¡Hola, DevOps!", App.saludar("DevOps"));
+    }
+}
